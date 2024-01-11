bab6b09196dab00dd6470270f9ce6a51c6c53bbd
Klinton Jay <48824774+Bratax@users.noreply.github.com>
2024-01-12 01:52:05 +0800

    Update LoginPage.js

--- frontend/src/pages/LoginPage.js
+++ frontend/src/pages/LoginPage.js
@@ -77,20 +77,20 @@ const LoginPage = ({ role }) => {
         const password = "zxc"
 
         if (role === "Admin") {
-            const email = "yogendra@12"
+            const email = "zoren@12"
             const fields = { email, password }
             setGuestLoader(true)
             dispatch(loginUser(fields, role))
         }
         else if (role === "Student") {
             const rollNum = "1"
-            const studentName = "Dipesh Awasthi"
+            const studentName = "zoren"
             const fields = { rollNum, studentName, password }
             setGuestLoader(true)
             dispatch(loginUser(fields, role))
         }
         else if (role === "Teacher") {
-            const email = "tony@12"
+            const email = "zoren@123"
             const fields = { email, password }
             setGuestLoader(true)
             dispatch(loginUser(fields, role))
